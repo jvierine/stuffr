18636d2741352c681e756759f22b1f9cc1e2e398
Vetle Hofsoy-Woie <vho023@uit.no>
2021-06-17 13:42:05 +0200

    Add tests for unix date conversion

---
+++ tests/test_unixdate.py
@@ -0,0 +1,25 @@
+import unittest
+import datetime
+
+from stuffr import unix2date, date2unix
+
+class TestUnixDate(unittest.TestCase):
+
+    def test_unix_to_date(self):
+        expected = datetime.datetime(1970,1,1,0,0)
+        date = unix2date(0)
+        self.assertEqual(expected, date)
+    
+    def test_date_to_unix(self):
+        expected = 0
+        timestamp = date2unix(1970,1,1,0,0,0)
+        self.assertEqual(expected, timestamp)
+    
+    def test_unix_to_date_to_unix(self):
+        expected = 52254455 #random number
+        date = unix2date(expected)
+        timestamp = date2unix(date.year, date.month, date.day, date.hour, date.minute, date.second)
+        self.assertEqual(expected, timestamp)
+        
+if __name__ == '__main__':
+    unittest.main()
